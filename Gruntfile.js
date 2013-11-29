7f3ed27703530112ad42973c446fca5cf2f6226b
Erik Anderberg <erikag@axis.com>
2013-11-29 15:38:51 +0100

    Moved services to own folder

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,7 +11,7 @@ module.exports = function(grunt) {
       
     // Task configuration.
     concat: {
-      dist: { src: ['src/js/*.js', 'src/js/Directives/*.js'], dest: 'dist/script.js' }
+      dist: { src: ['src/js/*.js', 'src/js/*/*.js'], dest: 'dist/script.js' }
     },
     uglify: {
       dist: { files: { 'dist/script.min.js': '<%= concat.dist.dest %>' } }
